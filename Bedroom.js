ca3dc79511811b9d4f69ac1d38c45fa082c88e2a
Poorvi1010 <91541629+Poorvi1010@users.noreply.github.com>
2022-07-27 18:05:37 +0530

    Add files via upload

--- Bedroom.js
+++ Bedroom.js
@@ -1,4 +1,5 @@
 img="";
+Status="";
 function preload()
 {
 img=loadImage("Bedroom.webp");
@@ -8,6 +9,25 @@ function setup()
 {
    canvas=createCanvas(640,420);
    canvas.center(); 
+   objectdetector= ml5.objectDetector("cocossd",ModelLoaded);
+   document.getElementById("status").innerHTML="Status : Detecting Object"
+
+}
+
+function ModelLoaded()
+{
+   console.log("Model Loaded");
+   Status= true;
+   objectdetector.detect(img,gotResults);
+}
+
+function gotResults(error,results)
+{
+if(error)
+{
+   console.log(error);
+}
+console.log(results);
 }
 
 function draw()
